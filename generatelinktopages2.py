6706fa92537b1878670db3dee0844943261e88ea
Jon Titor <admiralorbiter@gmail.com>
2020-11-01 19:06:19 -0600

    Adding stuff

---
+++ generatelinktopages2.py
@@ -0,0 +1,32 @@
+template = """
+<html>
+<head>
+<title>{title}</title>
+</head>
+<body>
+<center>
+<h1>
+{title}
+</h1>
+<h2>
+{mystring}
+</h2>
+</center>
+</body>
+</html>
+"""
+
+message=""
+
+for x in range(1, 14):
+    message=message+"<a href=""{url}.html"">{body}</a><br>".format(url="/emailoutgoingdata/week"+str(x), body="Week "+str(x))
+
+
+f = open("outgoingemaildata.html", "w")
+f.write(template.format(title="Outgoing Email Data", mystring=message))
+f.close()
+
+for x in range(1, 14):
+    f = open("week"+str(x)+".html", "w")
+    f.write(template.format(title="Week "+str(x)+" Outgoing Email Data", mystring='<img src="week'+str(x)+'.png">'))
+    f.close()
